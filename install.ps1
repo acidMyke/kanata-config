7d855d476e9412dcbc9fe5576a18db8c43b54640
acidMyke <me@acidmyke.link>
2024-11-03 20:22:57 +0800

    add download script for kanata-gui for windows

---
+++ install.ps1
@@ -0,0 +1,2 @@
+# Script to download not really install but download is too long to type in cli :D
+Invoke-WebRequest "https://github.com/jtroo/kanata/releases/latest/download/kanata_gui.exe" -OutFile "kanata-gui.exe"
